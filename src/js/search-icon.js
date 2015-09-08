e1ed75d848e1d735b775606e8d0778c758056993
pcsandford <petersandford1982@gmail.com>
2015-09-08 07:28:39 -0400

    updated commit - making icon X from start and and commenting out fucntionality

--- src/js/search-icon.js
+++ src/js/search-icon.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-$( ".search-button" ).on('click', function() {
-  $(".search-icon").toggleClass("open");
-});
+// $( ".search-button" ).on('click', function() {
+//   $(".search-icon").toggleClass("open");
+// });
 
-});
+// });
